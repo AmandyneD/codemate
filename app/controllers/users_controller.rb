c2c6a4b4d920bcafe5521249dcb90ed55d07da26
Amandyne Desfonds <amandyne.desfonds@gmail.com>
2026-03-10 09:15:27 +0100

    Ajout Technologies sur Edit Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [ :show, :edit, :update ]
+  before_action :authenticate_user!, only: [ :edit, :update ]
+  before_action :authorize_user!, only: [ :edit, :update ]
 
   def show
     @owned_projects = @user.collaborations
@@ -17,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user == current_user && @user.update(user_params)
+    if @user.update(user_params)
       redirect_to @user, notice: "Profile updated successfully."
     else
       render :edit, status: :unprocessable_entity
@@ -30,7 +32,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def authorize_user!
+    return if @user == current_user
+
+    redirect_to user_path(@user), alert: "You are not allowed to edit this profile."
+  end
+
   def user_params
-    params.require(:user).permit(:display_name, :bio, technology_ids: [])
+    params.require(:user).permit(
+      :display_name,
+      :bio,
+      :github_url,
+      :avatar_url,
+      technology_ids: []
+    )
   end
 end
